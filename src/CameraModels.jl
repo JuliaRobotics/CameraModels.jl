e074286e31e6959f27eed844b8ea47bf096b93c4
dehann <dehann.fourie@gmail.com>
2020-12-07 23:01:53 -0500

    starting to populate PinholeCamera

--- src/CameraModels.jl
+++ src/CameraModels.jl
@@ -1,11 +1,29 @@
 module CameraModels
 
+using StaticArrays
+
 export AbstractCameraModel
+export PinholeCamera
 
 abstract type AbstractCameraModel end
 
+
+"""
+    $TYPEDEF
+
+Pinhole Camera model is the most simplistic camera model assuming an ideal pinhole camera model.
+
+Notes
+- https://en.wikipedia.org/wiki/Pinhole_camera
+"""
 struct PinholeCamera <: AbstractCameraModel
-    K::SArray{Float64,2}
+  K::SMatrix{3,3,Float64}
 end
+
+centerHeight(pc::PinholeCamera) = pc.K[1,3]
+centerWidth(pc::PinholeCamera) = pc.K[2,3]
+focalHeight(pc::PinholeCamera) = pc.K[1,1]
+focalWidth(pc::PinholeCamera) = pc.K[2,2]
+
 
 end # module
